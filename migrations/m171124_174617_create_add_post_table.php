44896e7957875c04ca05d5b696205e04e78046eb
mark <markk.9964@gmail.com>
2017-11-28 00:02:43 +0300

    asdas

---
+++ migrations/m171124_174617_create_add_post_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `add_post`.
+ */
+class m171124_174617_create_add_post_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('add_post', [
+            'id' => $this->primaryKey(),
+            'user_id' =>$this->integer(),
+            'caption'=>$this->string(),
+            'searchCountry' =>$this->string(),
+            'searchRegion'=>$this->string(),
+            'searchCity'=>$this->string(),
+            'event'=>$this->string(),
+
+            'description'=>$this->text(),
+
+            'fotoFile'=>$this->string(),
+            'fotoDescr'=>$this->text(),
+
+            'videoFile'=>$this->string(),
+            'videoDescr'=>$this->text(),
+
+            'map'=>$this->string(),
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('add_post');
+    }
+}
